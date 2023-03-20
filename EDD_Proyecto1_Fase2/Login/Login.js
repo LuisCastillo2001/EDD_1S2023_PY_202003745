11d353d2d889cd3ff377edb98ce63a560e616c0d
LuisCastillo2001 <3020186890101@ingenieria.usac.edu.gt>
2023-03-20 13:52:25 -0600

    cambios

---
+++ EDD_Proyecto1_Fase2/Login/Login.js
@@ -0,0 +1,16 @@
+function Redirected(){
+    let usuario = document.getElementById("Usuario").value
+    let contraseña = document.getElementById("Contraseña").value
+    if (usuario == "admin"&& contraseña == "admin"){
+       
+        
+        let localización = window.location.href.replace("Login.html","otro.html")
+
+
+        console.log(localización)
+        window.alert("x")
+        window.location.href = localización
+    }else{
+        window.alert("no")
+    }
+}
