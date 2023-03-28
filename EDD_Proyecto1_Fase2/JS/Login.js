7e066f55629502ad9d97fafdc663ff351189995b
LuisCastillo2001 <3020186890101@ingenieria.usac.edu.gt>
2023-03-28 12:24:15 -0600

    Admin finalizado

--- EDD_Proyecto1_Fase2/JS/Login.js
+++ EDD_Proyecto1_Fase2/JS/Login.js
@@ -7,10 +7,50 @@ function Redirected(){
         let localización = window.location.href.replace("Login.html","Admin.html")
 
 
-        console.log(localización)
-        window.alert("x")
+        
         window.location.href = localización
     }else{
-        window.alert("no")
+        carnet = parseInt(usuario)
+        usuario_log = verificar(carnet,contraseña)
+        if (usuario_log === 1){
+            window.alert("Usuario encontrado con éxito")
+        }else{
+            window.alert("El usuario no existe")
+        }
     }
+}
+
+
+function verificar(carnet,contraseña){
+    arbol = JSON.parse(localStorage.getItem('arbol'))
+    carnet = carnet
+    contraseña = contraseña
+    usuario = recorridoInorden(arbol.raiz,carnet,contraseña)
+    if (usuario === 1){
+        return 1
+    }else{
+        return 0
+    }
+    
+}
+
+
+function recorridoInorden(raiz,carnet,contraseña){
+    var cadena = ""
+    if(raiz !== null){
+        if(raiz.izquierdo !== null){
+            cadena += this.recorridoInorden(raiz.izquierdo)
+            cadena += " -> "
+        }
+        cadena += "\""
+        cadena += raiz.carnet
+        if (raiz.carnet == carnet && raiz.contraseña == contraseña){
+            return 1
+        }
+        if(raiz.derecho !== null){
+            cadena += " -> "
+            cadena += this.recorridoInorden(raiz.derecho)
+        }
+    }
+    return 0
 }
