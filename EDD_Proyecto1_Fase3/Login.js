4f777ef9862d7aa44a980aaa292cf6759f797359
LuisCastillo2001 <3020186890101@ingenieria.usac.edu.gt>
2023-04-26 12:51:41 -0600

    agregando fase 3

---
+++ EDD_Proyecto1_Fase3/Login.js
@@ -0,0 +1,55 @@
+function Redirected(){
+    let usuario = document.getElementById("Usuario").value
+    let contraseña = document.getElementById("Contraseña").value
+    if (usuario == "admin"&& contraseña == "admin"){
+       
+        
+        let localización = window.location.href.replace("Login.html","Admin.html")
+
+
+        
+        window.location.href = localización
+    }else{
+        carnet = parseInt(usuario)
+        usuario_log = verificar(carnet,contraseña)
+        if (usuario_log !==null){
+            localStorage.setItem("usuario",JSON.stringify(usuario_log))
+            window.alert("Usuario encontrado con éxito")
+            
+            
+            let localización = window.location.href.replace("Login.html","Usuario.html")
+            window.location.href = localización
+        }else{
+            window.alert("El usuario no exsiste")
+        }
+    }
+}
+
+
+function verificar(carnet,contraseña){
+    arbol = JSON.parse(localStorage.getItem('arbol'))
+    carnet = carnet
+    contraseña = contraseña
+    usuario = buscar(arbol.raiz,carnet,contraseña)
+    if (usuario !== null){
+        return usuario
+    }else{
+        return null
+    }
+    
+}
+
+
+function buscar(nodo, carnet, contrasena) {
+    if (nodo === null) {
+      return null; 
+    } else if (carnet < nodo.carnet) {
+      return buscar(nodo.izquierdo, carnet, contrasena); 
+    } else if (carnet > nodo.carnet) {
+      return buscar(nodo.derecho, carnet, contrasena); 
+    } else if (contrasena === nodo.contraseña) {
+      return nodo; 
+    } else {
+      return null;
+    }
+  }
